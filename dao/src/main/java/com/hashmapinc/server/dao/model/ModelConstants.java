605c89913a66105b11bab7357d4fa51d03c7b898
shubhamkumargupta <shubhamkumargupta09@gmail.com>
2018-07-30 13:00:41 +0530

    adding NoSql Entity for CustomerGroup

--- dao/src/main/java/com/hashmapinc/server/dao/model/ModelConstants.java
+++ dao/src/main/java/com/hashmapinc/server/dao/model/ModelConstants.java
@@ -533,7 +533,7 @@ public class ModelConstants {
     /**
      * Cassandra CustomerGroup Constants
      */
-    public static final String CUSTOMER_GROUP_FAMILY_NAME = "customer_group";
+    public static final String CUSTOMER_GROUP_COLUMN_FAMILY_NAME = "customer_group";
     public static final String CUSTOMER_GROUP_TITLE_PROPERTY = TITLE_PROPERTY;
     public static final String CUSTOMER_GROUP_TENANT_ID_PROPERTY = TENANT_ID_PROPERTY;
     public static final String CUSTOMER_GROUP_CUSTOMER_ID_PROPERTY = CUSTOMER_ID_PROPERTY;
@@ -550,7 +550,7 @@ public class ModelConstants {
      * Cassandra CustomerGroup Policy Constants
      */
 
-    public static final String CUSTOMER_GROUP_POLICY_FAMILY_NAME = "customer_group_policy";
+    public static final String CUSTOMER_GROUP_POLICY_COLUMN_FAMILY_NAME = "customer_group_policy";
     public static final String CUSTOMER_GROUP_POLICY_ID_PROPERTY = "group_id";
     public static final String CUSTOMER_GROUP_POLICY_PROPERTY = "policy";
 
